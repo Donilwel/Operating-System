a4e1f10beb357f256125da9ab1e5c8dbb6ac2331
Maslow Danila Yurevich <134236921+Donilwel@users.noreply.github.com>
2024-03-19 03:12:09 +0300

    Update part2.cpp

--- ihw1/9point/part2.cpp
+++ ihw1/9point/part2.cpp
@@ -9,8 +9,32 @@
 #define FIFO_FILE "fifo"
 
 void findDifferences(char *str1, char *str2, char *output1, char *output2) {
-    // Обработка данных
-    // Ваша реализация
+    int ascii[256] = {0};
+
+    // Заполняем массив ascii с учетом символов из второй строки
+    for (int i = 0; i < strlen(str2); i++) {
+        ascii[(int)str2[i]] = 1;
+    }
+
+    int index1 = 0;
+    int index2 = 0;
+
+    // Находим символы, которых нет во второй строке
+    for (int i = 0; i < strlen(str1); i++) {
+        if (ascii[(int)str1[i]] == 0) {
+            output1[index1++] = str1[i];
+        }
+    }
+
+    // Находим символы, отсутствующие в первой строке
+    for (int i = 0; i < strlen(str2); i++) {
+        if (ascii[(int)str2[i]] == 0) {
+            output2[index2++] = str2[i];
+        }
+    }
+
+    output1[index1] = '\0';
+    output2[index2] = '\0';
 }
 
 int main() {
