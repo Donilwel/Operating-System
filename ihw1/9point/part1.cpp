951decb55f8b4d22b9a767daa3118d5102664bc7
Maslow Danila Yurevich <134236921+Donilwel@users.noreply.github.com>
2024-03-19 03:07:34 +0300

    Update part1.cpp

--- ihw1/9point/part1.cpp
+++ ihw1/9point/part1.cpp
@@ -1,29 +1,43 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 128
 #define FIFO_FILE "fifo"
 
-int main() {
-    mkfifo(FIFO_FILE, 0666);
-    int fd = open(FIFO_FILE, O_WRONLY);
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " input.txt output.txt" << std::endl;
+        return 1;
+    }
 
-    std::ifstream inputFile("input.txt");
+    std::string inputFileName = argv[1];
+    std::string outputFileName = argv[2];
+
+    std::ifstream inputFile(inputFileName);
     if (!inputFile) {
         std::cerr << "Error: Unable to open input file." << std::endl;
         return 1;
     }
 
-    char buffer[BUFFER_SIZE];
-    while (!inputFile.eof()) {
-        inputFile.read(buffer, BUFFER_SIZE);
-        write(fd, buffer, inputFile.gcount());
-    }
+    mkfifo(FIFO_FILE, 0666);
 
+    int fd = open(FIFO_FILE, O_WRONLY);
+    char buffer[BUFFER_SIZE];
+    inputFile.read(buffer, BUFFER_SIZE);
+    write(fd, buffer, inputFile.gcount());
     close(fd);
+
+    int fd2 = open(FIFO_FILE, O_RDONLY);
+    std::ofstream outputFile(outputFileName);
+    read(fd2, buffer, BUFFER_SIZE);
+    outputFile.write(buffer, strlen(buffer));
+    close(fd2);
+
     remove(FIFO_FILE);
 
     std::cout << "Process 1 finished" << std::endl;
