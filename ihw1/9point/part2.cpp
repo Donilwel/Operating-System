f862904c2e25ee493e92d1ae0ea9367a62898d1d
Maslow Danila Yurevich <134236921+Donilwel@users.noreply.github.com>
2024-03-19 03:09:21 +0300

    Update part2.cpp

--- ihw1/9point/part2.cpp
+++ ihw1/9point/part2.cpp
@@ -1,28 +1,62 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 128
 #define FIFO_FILE "fifo"
 
+void findDifferences(char *str1, char *str2, char *output1, char *output2) {
+    int ascii[256] = {0};
+
+    // Заполняем массив ascii с учетом символов из второй строки
+    for (int i = 0; i < strlen(str2); i++) {
+        ascii[(int)str2[i]] = 1;
+    }
+
+    int index1 = 0;
+    int index2 = 0;
+
+    // Находим символы, которых нет во второй строке
+    for (int i = 0; i < strlen(str1); i++) {
+        if (ascii[(int)str1[i]] == 0) {
+            output1[index1++] = str1[i];
+        }
+    }
+
+    // Находим символы, отсутствующие в первой строке
+    for (int i = 0; i < strlen(str2); i++) {
+        if (ascii[(int)str2[i]] == 0) {
+            output2[index2++] = str2[i];
+        }
+    }
+
+    output1[index1] = '\0';
+    output2[index2] = '\0';
+}
+
+
 int main() {
+    mkfifo(FIFO_FILE, 0666);
+
     int fd = open(FIFO_FILE, O_RDONLY);
-
-    std::ofstream outputFile("output.txt");
-    if (!outputFile) {
-        std::cerr << "Error: Unable to open output file." << std::endl;
-        return 1;
-    }
-
     char buffer[BUFFER_SIZE];
-    int bytesRead;
-    while ((bytesRead = read(fd, buffer, BUFFER_SIZE)) > 0) {
-        outputFile.write(buffer, bytesRead);
-    }
-
+    read(fd, buffer, BUFFER_SIZE);
     close(fd);
+
+    std::string data(buffer);
+
+    std::string output1, output2;
+    findDifferences(data, "", output1, output2);
+
+    int fd2 = open(FIFO_FILE, O_WRONLY);
+    write(fd2, output1.c_str(), output1.length() + 1);
+    close(fd2);
+
+    remove(FIFO_FILE);
 
     std::cout << "Process 2 finished" << std::endl;
 
